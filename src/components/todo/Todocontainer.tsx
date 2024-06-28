3faa9dd28f157250a56e283c56de53c0dd82efa2
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-28 15:56:10 +0600

    modal created

--- src/components/todo/Todocontainer.tsx
+++ src/components/todo/Todocontainer.tsx
@@ -1,9 +1,13 @@
+import AddTodo from "./AddTodo";
 import Todocard from "./Todocard";
 
 const TodoContainer = () => {
     return (
         <div>
-            <div className="bg-cyan-500 w-full rounded-2xl   p-5 mt-10">
+            <div>
+                <AddTodo></AddTodo>
+            </div>
+            <div className="bg-cyan-500 w-full rounded-2xl   p-5  my-5">
            <Todocard></Todocard>
            <Todocard></Todocard>
            <Todocard></Todocard>
