d3eb06652fbced594a43c449e3bfe2c8e1fb617f
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-30 18:20:46 +0600

    Redux Cache Behavio

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath:'baseapi',
      baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000'}),
-     tagTypes:['doto'],
+     tagTypes:['doto'],//26-4 video Redux Cache Behaviors
      endpoints:(builder)=>({
         getTodo:builder.query({
             query:()=>({
