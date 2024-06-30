baf4037dae0097124e45821594309da7610a790e
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-30 18:31:53 +0600

    Redux Cache Behavio

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,33 +2,37 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 export const baseApi = createApi({
-    reducerPath:'baseapi',
-     baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000'}),
-     tagTypes:['doto'],//26-4 video Redux Cache Behaviors
-     endpoints:(builder)=>({
-        getTodo:builder.query({
-            query:()=>({
-                url:'/tasks',
-                method:"GET"
-            }),
-            providesTags:['doto'],
-        }) ,
-        addTodo:builder.mutation({
-           
-            
-            query:(data)=>({
-                url:'/task',
-                method:"POST",
-                body:data
+    reducerPath: 'baseapi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    tagTypes: ['doto'],//26-4 video Redux Cache Behaviors
+    endpoints: (builder) => ({
+        getTodo: builder.query({
+            query: () => {
                 
+                return {
+                    url: '/tasks',
+                    method: "GET"
+                }
+                   
+            },
+ providesTags: ['doto'],
+        }),
+        addTodo: builder.mutation({
+
+
+            query: (data) => ({
+                url: '/task',
+                method: "POST",
+                body: data
+
             }),
-            invalidatesTags:['doto']
-           
+            invalidatesTags: ['doto']
+
         })
-     })
+    })
 
 })
 
 
 
-export const { useGetTodoQuery,useAddTodoMutation} = baseApi
+export const { useGetTodoQuery, useAddTodoMutation } = baseApi
