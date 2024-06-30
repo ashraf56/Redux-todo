4c6265677589f51c321a226fff9700d9599b1eac
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-30 18:39:49 +0600

    Filter todo ts created

---
+++ src/components/todo/Filtertodo.tsx
@@ -0,0 +1,33 @@
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+
+const Filtertodo = ({priority,setPriority}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="text-xl font-semibold">
+          Filter
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Filter by priority</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={priority} onValueChange={setPriority}>
+          <DropdownMenuRadioItem value="high">High</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="medium">Medium</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Filtertodo;
--- src/components/todo/Todocontainer.tsx
+++ src/components/todo/Todocontainer.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import AddTodo from "./AddTodo";
 import Todocard from "./Todocard";
  import { useGetTodoQuery } from "@/redux/api/api";
+import Filtertodo from "./Filtertodo";
 
 const TodoContainer = () => {
     // const todos = useAppSelector(state => state.todos.todos)
-
- const {data,isLoading} = useGetTodoQuery(undefined)
+ const [priority,setPriority] = useState('')
+ const {data,isLoading} = useGetTodoQuery(priority)
+console.log(priority);
 
  if (isLoading) {
     return <p>Loading.....</p>
@@ -14,8 +17,9 @@ const TodoContainer = () => {
 
     return (
         <div>
-            <div>
+            <div className="flex justify-between">
                 <AddTodo></AddTodo>
+                <Filtertodo priority={priority} setPriority={setPriority}></Filtertodo>
             </div>
             <div className="bg-cyan-500 w-full rounded-2xl   p-5  my-5">
           {
