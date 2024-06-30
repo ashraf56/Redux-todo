a7dfc4b90900bcf76f418a55d54a62a81f7d19d3
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-30 11:01:43 +0600

    Rtk query first implementation

---
+++ src/redux/api/api.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const baseApi = createApi({
+    reducerPath:'baseapi',
+     baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000'}),
+     endpoints:
+
+})
+
+
+
+export const { } = baseApi
